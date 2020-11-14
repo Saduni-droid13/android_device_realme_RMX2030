43147133c889a0e74b3b548a293bbae08814a9fb
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2020-11-14 16:04:42 +0700

    RMX1911: Update product props

--- product_prop.mk
+++ product_prop.mk
@@ -62,12 +62,15 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Radio
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.telephony.default_network=9,0 \
+    DEVICE_PROVISIONED=1 \
+    ro.telephony.default_network=22,18 \
     persist.vendor.radio.lte_vrte_ltd=1 \
     persist.vendor.radio.cs_srv_type=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.force_on_dc=true \
-    persist.vendor.radio.redir_party_num=1
+    persist.vendor.radio.redir_party_num=1 \
+    ril.subscription.types=NV,RUIM \
+    telephony.lteOnCdmaDevice=1
 
 # UI
 PRODUCT_PRODUCT_PROPERTIES += \
