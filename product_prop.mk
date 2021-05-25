366beba67d8bcff09753105589e91047da156826
baalajimaestro <me@baalajimaestro.me>
2021-05-25 12:44:59 +0000

    RMX2030: Optimise dex flags for a faster boot
    * Used multiple threads and speed profile to hasten the first boot
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Change-Id:
    I2cce5ddf7d50308511e81436fcac613b2c6537bf

--- product_prop.mk
+++ product_prop.mk
@@ -32,6 +32,17 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.charger.enable_suspend=true
 
+# Dex
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    pm.dexopt.boot=verify
+    pm.dexopt.first-boot=verify
+    pm.dexopt.install=speed-profile
+    dalvik.vm.image-dex2oat-filter=speed
+    dalvik.vm.image-dex2oat-threads=8
+    dalvik.vm.dex2oat-filter=speed
+    dalvik.vm.dex2oat-threads=8
+    dalvik.vm.dex2oat64.enabled=true
+
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.hw=1 \
